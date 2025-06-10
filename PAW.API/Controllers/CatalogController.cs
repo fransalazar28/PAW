7b5683406b2a8f58ccffd43e2d4c7e7a0ff02b50
fransalazar28 <fsalazar50156@ufide.ac.cr>
2025-06-10 14:57:49 -0600

    Agregar archivos de proyecto.

---
+++ PAW.API/Controllers/CatalogController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PAW.Business;
+using PAW.Models;
+
+namespace PAW.API.Controllers;   // ← file-scoped namespace (C# 10+)
+
+[ApiController]
+[Route("[controller]")]
+public class CatalogController(IBusinessCatalog businessCatalog) : Controller
+{
+    [HttpGet(Name = "GetCatalogs")]
+    public async Task<IEnumerable<Catalog>> GetAll()
+    {
+        return await businessCatalog.GetAllCatalogsAsync();
+    }
+
+    /* [HttpGet(Name = "GetCatalogById")]
+     public async Task<Catalog> Get([FromQuery] int id)
+     {
+        var catalog = await businessCatalog.GetCatalogAsync(id);
+         return catalog;
+     }*/
+
+    //Fix
+
+    [HttpGet("{id:int}", Name = "GetCatalogById")]
+    public async Task<ActionResult<Catalog>> GetById(int id)
+    {
+        var catalog = await businessCatalog.GetCatalogAsync(id);
+        return catalog;
+    }
+
+		
+    [HttpPost]
+    public async Task<bool> Save([FromBody] Catalog catalog)
+    {
+        return await businessCatalog.SaveCatalogAsync(catalog);
+    }
+
+    [HttpDelete]
+    public async Task<bool> Delete(Catalog catalog)
+    {
+        return await businessCatalog.DeleteCatalogAsync(catalog);
+    }
+}
---
+++ PAW.Repositories/RepositoryCatalog.cs
@@ -0,0 +1,37 @@
+﻿using PAW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAW.Repositories
+{
+    public interface IRepositoryCatalog
+    {
+  
+        Task<bool> UpsertAsync(Catalog entity, bool isUpdating);
+        Task<bool> CreateAsync(Catalog entity);
+        Task<bool> DeleteAsync(Catalog entity);
+        Task<IEnumerable<Catalog>> ReadAsync();
+        Task<Catalog> FindAsync(int id);
+        Task<bool> UpdateAsync(Catalog entity);
+        Task<bool> UpdateManyAsync(IEnumerable<Catalog> entities);
+        Task<bool> ExistsAsync(Catalog entity);
+        Task<bool> CheckBeforeSavingAsync(Catalog entity);
+    }
+
+    public class RepositoryCatalog : RepositoryBase<Catalog>, IRepositoryCatalog
+    {
+        public async Task<bool> CheckBeforeSavingAsync(Catalog entity)
+        {
+            var exists = await ExistsAsync(entity);
+           if (exists)
+            {
+                // algo mas
+            }
+
+           return await UpsertAsync(entity,exists);
+        }
+    }
+}
